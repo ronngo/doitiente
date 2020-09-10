5a27556f8cfb11aa403e78888b3966d890b92363
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-10 21:51:45 +0700

    Add files via upload

---
+++ doitiente/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace chuyendoitiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Nhập tỉ giá tiền :");
+            int tigia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập USD :");
+            int usd = int.Parse(Console.ReadLine());
+            doitien(tigia, usd);
+        }
+        static int doitien(int tigia, int usd)
+        {
+            int vnd = tigia * usd;
+            Console.WriteLine("USD => VNĐ là : {0} Vnđ", vnd);
+            return vnd;
+        }
+    }
+}
